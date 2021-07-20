c6d042748b112ba80736c79b05fda4202667a995
Ragav Kumar <ragavkumarv@gmail.com>
2021-07-20 12:16:04 +0530

    Get by id -  poll

--- index.js
+++ index.js
@@ -43,7 +43,35 @@ app.get("/poll", (request, response) => {
   response.send(poll);
 });
 
+app.get("/poll/:id", (request, response) => {
+  const id = request.params.id;
+  console.log(id);
+  response.send(id);
+});
+
 app.listen(PORT, () => console.log("The server is started in ", PORT));
 
 // ctrl+c -> stop server
 // stop it & rerun the server
+
+// UI (database query are here) -> Database
+// React -> Vue
+// Vue (rewrite) -> Database
+// UI  -> Node (database query are here)   -> Database
+// New UI -> Node (database query are here)   -> Database
+
+// Business
+// Node (database query are here) (public)  -> Database
+// 1. Mock api -> pro -> pay
+// 2. Indigo flight (/booktickets) -> Charging ->  BookmyFlight, MakeMytrip, paytm
+// 3. Indigo flight (/currentTicketCost) -> Charging -> MakeMytrip
+
+// Multiple platforms
+// Mobile app, android, ios, Webapp(React), Windows, MacOS -> Node (database query are here)   -> Database
+
+// Slack -> android, ios, Webapp(React), Windows, MacOS
+
+// REST -> Everyone speaks the same language
+
+// REST methods
+// GET, POST, PUT, DELETE
