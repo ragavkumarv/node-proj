681e134b93527c4952999500b3979c4d8ae75a15
Ragav Kumar <ragavkumarv@gmail.com>
2021-07-20 10:52:32 +0530

    max number soultion

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-console.log("Put your code in this file");
+console.log("Put your code in this file", process.argv);
+const stringArr = process.argv[2];
+const arr = JSON.parse(stringArr);
 
-console.log("Max number is: ");
+console.log(arr, typeof arr);
+// let max = arr[0];
+
+// arr.forEach((num) => {
+//   if (max < num) {
+//     max = num;
+//   }
+// });
+
+console.log(Math.max(...arr));
+
+// console.log("Max number is: ", max);
