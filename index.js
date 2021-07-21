1e8b35647aa11b023a33c750c35f187daa0619a2
Ragav Kumar <ragavkumarv@gmail.com>
2021-07-21 11:32:08 +0530

    get by id

--- index.js
+++ index.js
@@ -3,61 +3,69 @@ const { MongoClient } = require("mongodb");
 const app = express();
 const PORT = 5000;
 
-const poll = [
-  {
-    id: "4",
-    company: "Oneplus",
-    color: "red",
-    content: "China based company",
-  },
-  {
-    id: "2",
-    company: "Samsung",
-    color: "skyblue",
-    content: "Korean based company!",
-  },
+// const poll = [
+//   {
+//     id: "4",
+//     company: "Oneplus",
+//     color: "red",
+//     content: "China based company",
+//   },
+//   {
+//     id: "2",
+//     company: "Samsung",
+//     color: "skyblue",
+//     content: "Korean based company!",
+//   },
 
-  {
-    id: "5",
-    company: "Moto",
-    color: "#000080",
-    content: "US based company",
-  },
-  {
-    color: "pink",
-    content: "India Based Company",
-    company: "oppo",
-    id: "6",
-  },
+//   {
+//     id: "5",
+//     company: "Moto",
+//     color: "#000080",
+//     content: "US based company",
+//   },
+//   {
+//     color: "pink",
+//     content: "India Based Company",
+//     company: "oppo",
+//     id: "6",
+//   },
 
-  {
-    id: "3",
-    company: "MI",
-    color: "orange",
-    content: "China based company",
-  },
-];
+//   {
+//     id: "3",
+//     company: "MI",
+//     color: "orange",
+//     content: "China based company",
+//   },
+// ];
 
 async function createConnection() {
   const MONGO_URL =
-    "mongodb+srv://ragavkumarv:<pass>@cluster0.yn2hm.mongodb.net/contestants?retryWrites=true&w=majority";
-
+    "mongodb+srv://ragavkumarv:pass@cluster0.yn2hm.mongodb.net/contestants?retryWrites=true&w=majority";
   const client = new MongoClient(MONGO_URL);
-
   try {
     await client.connect();
-
-    const result = await client
-      .db("contestants")
-      .collection("poll")
-      .insertMany(poll);
-
-    console.log("Inserted successfully", result);
-
-    console.log("Succesfully connected");
+    return client;
+    // getPollById(client, "4");
   } catch (err) {
     console.log(err);
   }
+}
+
+async function getPollById(client, id) {
+  const result = await client
+    .db("contestants")
+    .collection("poll")
+    .findOne({ id: id });
+  console.log("Succesfully connected", result);
+  return result;
+}
+
+async function insertPoll(client, poll) {
+  const result = await client
+    .db("contestants")
+    .collection("poll")
+    .insertMany(poll);
+  console.log("Inserted successfully", result);
 }
 
 createConnection();
@@ -70,11 +78,14 @@ app.get("/poll", (request, response) => {
   response.send(poll);
 });
 
-app.get("/poll/:id", (request, response) => {
+app.get("/poll/:id", async (request, response) => {
   const id = request.params.id;
 
-  const contestant = poll.filter((data) => data.id === id);
-  console.log(id, contestant);
+  // const contestant = poll.filter((data) => data.id === id);
+  // console.log(id, contestant);
+  const client = await createConnection();
+  const contestant = await getPollById(client, id);
+
   response.send(contestant);
 });
 
