56b2253e12427cb96f58661d3c58d4f34a610e5e
Ragav Kumar <ragavkumarv@gmail.com>
2021-07-23 12:04:14 +0530

    Login & Get users

--- helper.js
+++ helper.js
@@ -42,3 +42,26 @@ export async function insertUser(client, user) {
   console.log("Inserted successfully", result);
   return result;
 }
+
+export async function getUsers(client, filter) {
+  const result = await client
+    .db("contestants")
+    .collection("user")
+    .find(filter)
+    .toArray();
+
+  // db.poll.find({color: 'green'})
+  console.log("Succesfully connected", result);
+  return result;
+}
+
+export async function getUser(client, filter) {
+  const result = await client
+    .db("contestants")
+    .collection("user")
+    .findOne(filter);
+
+  // db.poll.find({color: 'green'})
+  console.log("Succesfully connected", result);
+  return result;
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 import bcrypt from "bcrypt";
 import express from "express";
-import { insertUser } from "../helper.js";
+import { getUser, getUsers, insertUser } from "../helper.js";
 import { createConnection } from "../index.js";
 
 const router = express.Router();
+
+// get all users
+router.route("/").get(async (request, response) => {
+  const client = await createConnection();
+  const contestants = await getUsers(client, {});
+  response.send(contestants);
+});
 
 // user password - create a user
 router.route("/signup").post(async (request, response) => {
@@ -15,8 +22,24 @@ router.route("/signup").post(async (request, response) => {
     password: hashedPassword,
     avatar,
   });
-  console.log(hashedPassword, newUser);
+  // console.log(hashedPassword, newUser);
   response.send(newUser);
+});
+
+// Make check if user already exists - task
+
+// login
+router.route("/login").post(async (request, response) => {
+  const { username, password } = request.body;
+  const client = await createConnection();
+  const user = await getUser(client, { username: username });
+  const inDbStorePassword = user.password;
+  const isPasswordMatch = await bcrypt.compare(password, inDbStorePassword);
+  if (isPasswordMatch) {
+    response.send({ message: "Successfull login" });
+  } else {
+    response.send({ message: "Invalid login" });
+  }
 });
 
 // list all the users in database
